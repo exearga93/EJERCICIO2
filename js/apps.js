bcda43ee56a14cb4e50e4b1ce7a81bfcca00c8f2
Exequiel Argañaraz <exequiel.it93@gmail.com>
2024-11-18 10:50:04 -0300

    se inicia el maquetado del ejercicio 2

---
+++ js/apps.js
@@ -0,0 +1,11 @@
+class Persona{
+    constructor(nombre, edad, dni, genero, peso, altura, fechaNacimiento){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.dni=dni;
+        this.genero=genero;
+        this.peso=peso;
+        this.altura=altura;
+        this.fechaNacimiento=fechaNacimiento;
+    }
+}
